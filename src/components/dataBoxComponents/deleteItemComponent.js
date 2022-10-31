51b14ffe501692ddebedc11e82f73ec25e61de7f
Patryk Romanowski <patrykromanowski90@gmail.com>
2022-10-31 23:05:43 +0100

    build delete page

--- src/components/dataBoxComponents/deleteItemComponent.js
+++ src/components/dataBoxComponents/deleteItemComponent.js
@@ -2,9 +2,35 @@ import React from "react";
 
 import classes from "./deleteItemComponent.module.css";
 
+import { useSelector, useDispatch } from "react-redux";
+
+import CardOfDeleteItem from "../../layout/cardOfDeleteItem";
+import deleteIcon from "../../assets/icons/delete.png";
+
+import Card from "../../layout/Card";
+
 const DeleteItem = () => {
+  const myBoxes = useSelector((state) => state.itemsData.boxes);
+
+  console.log(myBoxes);
+
+  const showItemsForDelete = myBoxes.map((item) => (
+    <div className={classes.itemContainer}>
+      <div>{item.boxName}</div>
+      <CardOfDeleteItem items={item.items} />
+      <div className={classes.itemForDelete}>
+        <button className={classes.deleteItemButton}>
+          usuń
+          <img className={classes.deleteIcon} src={deleteIcon}></img>
+        </button>
+      </div>
+    </div>
+  ));
+
   return (
     <div>
+      {showItemsForDelete}
+
       <p>Delete page</p>
     </div>
   );
---
+++ src/layout/cardOfDeleteItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "./cardOfdeleteItem.module.css";
+
+const CardOfDeleteItem = (props) => {
+  return (
+    <div className={classes.card}>
+      <div></div>
+    </div>
+  );
+};
+
+export default CardOfDeleteItem;
