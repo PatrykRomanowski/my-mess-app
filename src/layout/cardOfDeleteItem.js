378738f8fb309a43c6ff05607f4176057391c7fd
Patryk Romanowski <patrykromanowski90@gmail.com>
2022-11-01 20:27:13 +0100

    displaing the list of items to be removed

--- src/layout/cardOfDeleteItem.js
+++ src/layout/cardOfDeleteItem.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import classes from "./cardOfdeleteItem.module.css";
 
 const CardOfDeleteItem = (props) => {
-  return (
-    <div className={classes.card}>
-      <div></div>
-    </div>
-  );
+  const [items, setItems] = useState([{}]);
+
+  useEffect(() => {
+    const myItemsArray = [{}];
+
+    for (const key in props.items) {
+      myItemsArray.push({ name: props.items[key].nameItem, id: key });
+    }
+    setItems(myItemsArray);
+    console.log(myItemsArray);
+    // console.log(myItems);
+  }, []);
+
+  console.log(items);
+
+  const itemsList = items.map((item) => {
+    console.log(item.name);
+    return (
+      <div>
+        <div>{item.name}</div>
+      </div>
+    );
+  });
+
+  return <div>{itemsList}</div>;
 };
 
 export default CardOfDeleteItem;
